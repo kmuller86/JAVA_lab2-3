4d358048100026a358e759f1fba2b17c3aeb4599
mikolajS95 <98489236+mikolajS95@users.noreply.github.com>
2022-01-26 23:48:31 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,108 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        /* ćwiczenie 5 Metody */
 
-        /* ćw.2 pętle for */
+        /* zad.1
+        a) stworzyć tablicę typu int[20],
+        b) napisać metodę która będzie uzupełniała tablicę wartościami od 40 do 21,
+        c) napisać metodę, która wypisze otrzymane wartości,
+        d) wstawić wywołanie metody do metody main
+        */
 
-        /* zad.1 Napisz program który będzie iterował od 100 do 0,
-         *  i wyświetlał liczby które są podzielne przez ostatnią cyfrę waszego indeksu
-         * (jesli tą liczbą jest 0 to przez 2 ostatnie) */
-        System.out.println("Zadanie 1: ");
+        int[] tab= new int [20];
+        uzupelnijTablice(tab);
 
-        int ostatniaCyfraIndexu =5;
 
-        for (int i= 100; i>0; i--)
-        {
-            System.out.print(i%ostatniaCyfraIndexu==0 ? "\nLiczba " + i + " jest podzielna przez ostatnią cyfrę indexu (czyli 5)" : "");
-        }
+        /* zad.2
+        a) stworzyc tablicę typu String[6],
+        b) napisać metodę która uzupełni tablicę wartościami podawanymi z klawiatury
+            (użyć do tego pętli i klasy Scanner),
+        c) napisać metodę która wyświetli elementy tablicy od ostatniego elementu do elementu o indeksie 0,
+        */
 
-        /* zad.2 Napisz program który będzie uzupełniał tablicę 5-cio elementową int[] liczbami
-         * wprowadzanymi z klawiatury (użyć klasy Scanner), a następnie będzie wyświetlał elementy tablicy
-         * za pomocą pętli forEach powiększone o 11 */
+        String[] tabString = new String[6];
 
-        System.out.println("\n\nZadanie 2: ");
+        uzupelnijWartosciamiZKlawiatury(tabString);
 
-        Scanner scan  = new Scanner(System.in);
-        int tab[] = new int[5];
+        wyswietlTablice(tabString);
 
-        for (int x=0; x<5;x++)
-        {
-            System.out.println("\nPodaj liczbę: ");
-            int liczba = scan.nextInt();
 
-            tab[x]= liczba;
-        }
+        /* zad.3
+        a) napisać metodę która przyjmuje 1 parametr typu String, metoda nic nie zwraca, tylko
+            wyświetla w konsoli string który ma być waszym imieniem,
+        b) napisać metodę o tej samej nazwie jak w punkcie a) z 2-oma parametrami typu String, która wyświetli
+            wasze imie i nazwisko,
+        c) napisać metodę o tej samej nazwie jak w punkcie a) i b) która przyjmuje wasze imie,
+            nazwisko i wiek (int), a następnie wszystko wyświetli,
+        */
+        przedstawSie("Mikołaj");
+        przedstawSie("Mikołaj", "Sykucki");
+        przedstawSie("Mikołaj", "Sykucki", 26);
 
-        for (int z : tab)
-        {
-            System.out.println(z + 11);
-        }
+
+        /* zad.4
+        a) napisać metodę która przyjmuje 2 parametry typu int i zwraca ich sumę, którą przypiszemy do zmiennej,
+            coś takiego:                                    zmiennaA = metoda(1, 2); <-- wywołanie metody
+        b) napisać metodę (nazwa ta sama co w punkcie a) która przyjmuje 3 parametry typu int i zwraca ich sumę,
+            którą przypiszemy do zmiennej, coś takiego:     zmiennaB = metoda(7, 8, 9); <-- wywołanie metody
+        c) wyświetlić sumę wywołań obu metod ---> print("wynik: " + (metoda(8, 9) + metoda(2, 3, 4));
+        */
+        int suma = dodawanie(1,2);
+
+        int sumaZTrzech = dodawanie(1, 2, 3);
+
+        System.out.println("wynik: " + (dodawanie(1,2) + dodawanie(1, 2, 3)));
+    }
+ public static void uzupelnijTablice(int tab[])
+ {
+   for (int i=0;i< tab.length;i++)
+   {
+    tab[i]=40-i;
+
+   }
+
+ }
+ public static void uzupelnijWartosciamiZKlawiatury(String tabString[])
+ {
+     Scanner scan = new Scanner(System.in);
+
+     for (int i=0;i< tabString.length;i++)
+     {
+         System.out.println("Podaj słowo:");
+          tabString[i]= scan.nextLine();
+     }
+ }
+public static void wyswietlTablice(String tabString[])
+{
+    for (int i=(tabString.length-1);i>=0;i--)
+    {
+        System.out.print(tabString[i] + " ");
+    }
+    System.out.println();
+}
+public static void przedstawSie(String imie)
+{
+    System.out.println(imie);
+}
+    public static void przedstawSie(String imie, String nazwisko)
+    {
+        System.out.println(imie + " " + nazwisko);
+    }
+    public static void przedstawSie(String imie, String nazwisko, int wiek)
+    {
+        System.out.println(imie + " " + nazwisko + " " + wiek);
+    }
+public static int dodawanie(int x, int y)
+{
+    return x+y;
+}
+    public static int dodawanie(int x, int y, int z)
+    {
+        return x+y+z;
     }
 }
